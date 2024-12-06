7b7dd184b280924aedbc473f60b837c0bf235d1f
navya0980 <navyakeerthi95532@gmail.com>
2024-12-06 17:02:31 +0530

    add

--- Our_Project/public/javascripts/getProducts.js
+++ Our_Project/public/javascripts/getProducts.js
@@ -1,34 +1,49 @@
 var singleTemplate;
 document.addEventListener("DOMContentLoaded",()=>{
-   
-    axios.get("/templates/productDetails.htm").then((res)=>{
+    axios.get('/templates/productDetails.htm').then((res)=>{
         singleTemplate=Handlebars.compile(res.data);
-      
-        
-    })
+
 })
+
+})
+
+var products=[];
 var loadSingle=(product)=>{
-    
-    console.log(product)
-    $("#detailsBlock").append(singleTemplate(product));
-    ratingStar(product.product_id,product.rating);
- }
+   
+   $("#detailsBlock").append(singleTemplate(product))
+}
 
 var getProducts=()=>{
-    var products=[];
-    axios.get("/getProduct/data",).then((res)=>{
-        products=res.data;
-        $("#detailsBlock").html("");
-        products.forEach((product,index) => {
-            
-            loadSingle(product);
-            
-        });
-        
-
-    }).catch((err)=>{
-
-    });
-        
     
-}
+        axios.get('/getProduct/data').then((result)=>{
+            console.log(result.data);
+            products=result.data;
+            $("#detailsBlock").html("");
+            products.forEach((product,index)=>{
+
+               loadSingle(product);
+               ratingStar(product.product_id,product.rating);
+            })
+    
+        }).catch(()=>{
+
+        })
+       
+    }
+    // var apply=()=>{
+    //     var selected=document.querySelectorAll("[name=productsCat]:checked");
+    //     var categoryList=[];
+    //     selected.forEach((item)=>{
+    //        categoryList.push( item.getAttribute("value"));
+    //     })
+    //     axios.get("/getproduct/data",{params:{"category":categoryList}}).then((result)=>{
+    //         console.log(result.data)
+    //         products=result.data;
+    //         $(".products").html("");
+    //         products.forEach((product,index)=>{
+    //            loadSingle(product);
+    //         })
+    //     }).catch((err)=>{
+            
+    //     })
+    // }
--- Our_Project/public/javascripts/signup.js
+++ Our_Project/public/javascripts/signup.js
@@ -8,7 +8,7 @@ var UserSignUp=()=>{
     axios.post("/new/signup",userdetails).then((result)=>{
         if(result.data.msg=="Success"){
             $("#signupstatusmsg").text("You are registered Successfully!!");
-           
+           $("#register").hide();
           
            setTimeout(()=>{
             signupModal.hide();
